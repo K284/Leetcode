e78f2a52164d57d37c4eb5b452053049c37ec7dd
Krisha <134706894+K284@users.noreply.github.com>
2024-12-21 22:05:27 +0530

    Update 2872. Maximum Number of K-Divisible Components.java

--- 2872. Maximum Number of K-Divisible Components.java
+++ 2872. Maximum Number of K-Divisible Components.java
@@ -48,3 +48,43 @@ values.length == n
 Sum of values is divisible by k.
 The input is generated such that edges represents a valid tree.
 */
+class Solution {
+    public int maxKDivisibleComponents(int n, int[][] edges, int[] vals, int k) {
+        if (n < 2) return 1;
+        
+        List<List<Integer>> graph = new ArrayList<>();
+        for (int i = 0; i < n; i++) graph.add(new ArrayList<>());
+        int[] degree = new int[n];
+        
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+            degree[edge[0]]++;
+            degree[edge[1]]++;
+        }
+        
+        long[] nodeVals = new long[n];
+        for (int i = 0; i < n; i++) nodeVals[i] = vals[i];
+        Queue<Integer> leafQ = new LinkedList<>();
+        for (int i = 0; i < n; i++) if (degree[i] == 1) leafQ.add(i);
+        
+        int compCnt = 0;
+        while (!leafQ.isEmpty()) {
+            int curr = leafQ.poll();
+            degree[curr]--;
+            long carry = 0;
+            
+            if (nodeVals[curr] % k == 0) compCnt++;
+            else carry = nodeVals[curr];
+            
+            for (int nbr : graph.get(curr)) {
+                if (degree[nbr] == 0) continue;
+                degree[nbr]--;
+                nodeVals[nbr] += carry;
+                if (degree[nbr] == 1) leafQ.add(nbr);
+            }
+        }
+        
+        return compCnt;
+    }
+}
